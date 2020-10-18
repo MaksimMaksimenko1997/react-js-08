89755da9ae1b63295b641c345496b7aafbc9f8d0
MaksimMaksimenko1997 <59928413+MaksimMaksimenko1997@users.noreply.github.com>
2020-10-18 10:43:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Appbar from './components/Appbar/Appbar';
+import Login from './views/Login/Login';
+import HomeView from './views/HomeView/HomeView';
+import Signup from './views/Signup/Signup';
+import Contacts from './views/Contacts/Contacts';
+import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+import authOperations from './redux/auth/auth-operations';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.onGetCurrentUser();
+  }
+
+  render() {
+    return (
+      <>
+        <Appbar />
+        <Switch>
+          <PublicRoute exact path="/" component={HomeView} />
+          <PublicRoute
+            path="/signup"
+            restricted
+            redirectTo="/phonebook"
+            component={Signup}
+          />
+          <PublicRoute
+            path="/login"
+            restricted
+            redirectTo="/phonebook"
+            component={Login}
+          />
+          <PrivateRoute
+            path="/phonebook"
+            redirectTo="/login"
+            component={Contacts}
+          />
+        </Switch>
+      </>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = {
+  onGetCurrentUser: authOperations.getCurrentUser,
+};
+
+export default connect(null, mapDispatchToProps)(App);
